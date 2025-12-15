d2db762d93d4de17e127f6630583af143c432740
David Pury <118439772+davd5208@users.noreply.github.com>
2025-12-15 18:11:02 +0100

    pozadie+animacia
    pozadie nedokonce, su tam zuby a treaba vymysliet ako na bushes a trees

--- Assets/PlayerMovement.cs.cs
+++ Assets/PlayerMovement.cs.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -12,10 +14,20 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Animator _animator;
 
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
+
+            if (horizontal != 0)
+             {
+                 _animator.SetBool("isRunning", true);
+             }
+             else
+             {
+                 _animator.SetBool("isRunning", false);
+             }
 
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
