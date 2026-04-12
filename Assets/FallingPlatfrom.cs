f3dc3b5a2d158439e576841368b58d6c11865781
Kango <159473005+SamuelKovac@users.noreply.github.com>
2026-04-12 15:31:40 +0200

    Pridanie plošiny s idiomami + generacia falling platforiem

--- Assets/FallingPlatfrom.cs
+++ Assets/FallingPlatfrom.cs
@@ -1,23 +1,25 @@
-using UnityEngine;
-using System.Collections.Generic;
+﻿using UnityEngine;
 using System.Collections;
 
 public class FallingPlatfrom : MonoBehaviour
 {
     public float fallWait = 2f;
     public float destroyWait = 1f;
+    public float respawnWait = 3f;
 
     bool isFalling;
     Rigidbody2D rb;
+    Vector3 startPosition;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        startPosition = transform.position;
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(!isFalling && collision.gameObject.CompareTag("Player"))
+        if (!isFalling && collision.gameObject.CompareTag("Player"))
         {
             StartCoroutine(Fall());
         }
@@ -28,6 +30,23 @@ public class FallingPlatfrom : MonoBehaviour
         isFalling = true;
         yield return new WaitForSeconds(fallWait);
         rb.bodyType = RigidbodyType2D.Dynamic;
-        Destroy(gameObject, destroyWait);
+        yield return new WaitForSeconds(destroyWait);
+
+        gameObject.SetActive(false);
+
+        // Respawn riadi externý manager
+        RespawnManager.GetInstance().ScheduleRespawn(this, respawnWait);
     }
+
+public void Respawn()
+{
+    transform.position = startPosition;
+    transform.rotation = Quaternion.identity;
+    rb.bodyType = RigidbodyType2D.Dynamic;
+    rb.linearVelocity = Vector2.zero;
+    rb.angularVelocity = 0f;
+    rb.bodyType = RigidbodyType2D.Static;
+    isFalling = false;
+    gameObject.SetActive(true);
+}
 }
